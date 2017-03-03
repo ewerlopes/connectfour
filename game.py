24f9a57a86a9102101a5ddba5c4da3493c81df0a
EpocDotFr <contact@epoc.fr>
2017-03-03 08:42:02 +0100

    Thiiings

--- game.py
+++ game.py
@@ -12,21 +12,22 @@ class Game:
 
         self.window.fill((0, 0, 0))
 
+        self.board_cell = pygame.image.load(os.path.join('images', 'board_cell.png')).convert_alpha()
+
     def play(self):
         for event in pygame.event.get():
             utils.try_quit(event)
 
         chips = pygame.sprite.Group()
 
-        caca = RedChip()
-        caca.add(chips)
+        red_chip = RedChip()
+        red_chip.add(chips)
         chips.draw(self.window)
-
-        board_cell = pygame.image.load(os.path.join('images', 'board_cell.png')).convert_alpha()
 
         for x in range(0, config.COLS):
             for y in range(0, config.ROWS):
-                self.window.blit(board_cell, (x * config.IMAGES_SIDE_SIZE, y * config.IMAGES_SIDE_SIZE))
+                self.window.blit(self.board_cell, (x * config.IMAGES_SIDE_SIZE, y * config.IMAGES_SIDE_SIZE))
 
         pygame.display.update()
+
         self.clock.tick(config.FPS)
--- run.py
+++ run.py
@@ -1,14 +1,36 @@
 import pygame
 import config
 import game
+import logging
+import sys
+import os
+
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%d/%m/%Y %H:%M:%S',
+    stream=sys.stdout
+)
+
+logging.getLogger().setLevel(logging.INFO)
+
+logging.info('Initializing PyGame/{} (with SDL/{})'.format(
+    pygame.version.ver,
+    '.'.join(str(v) for v in pygame.get_sdl_version())
+))
 
 pygame.init()
+
+logging.info('Initializing main window')
 
 pygame.display.set_caption('Connect Four')
 
 clock = pygame.time.Clock()
 window = pygame.display.set_mode(config.WINDOW_SIZE)
 game = game.Game(window, clock)
+
+pygame.display.set_icon(pygame.image.load(os.path.join('images', 'icon.png')).convert_alpha())
+
+logging.info('Running the game')
 
 while True:
     game.play()
