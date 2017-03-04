11c77692e0f382514a9a0a36cd94419a5a9c177e
EpocDotFr <contact@epoc.fr>
2017-03-04 19:25:38 +0100

    Fix the Mac build script

--- build_mac.sh
+++ build_mac.sh
@@ -1,11 +1,11 @@
 # Shell script to build the Connect Four Mac OS executable.
 # The resulting "connectfour.app" executable and "connectfour" script will be available in the "dist" directory.
 
-pyinstaller ^
+pyinstaller \
     --clean --noconfirm --onefile --windowed \
     --log-level=WARN \
     --name=connectfour \
     --icon="resources/images/icon.icns" \
-    --add-data="resources;resources" \
-    --osx-bundle-identifier-"fr.epoc.python.games.connectfour" \
+    --add-data="resources:resources" \
+    --osx-bundle-identifier="fr.epoc.python.games.connectfour" \
     run.py
