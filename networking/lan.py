67497cf07d2abbba091cf324a91f95f7683ddfee
EpocDotFr <contact@epoc.fr>
2017-03-17 17:35:48 +0100

    LAN

--- networking/lan.py
+++ networking/lan.py
@@ -4,6 +4,18 @@ import threading
 import constants
 
 
+class StoppableThread(threading.Thread):
+    def __init__(self):
+        super(StoppableThread, self).__init__()
+        self._stop = threading.Event()
+
+    def stop(self):
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.isSet()
+
+
 class Announce(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
@@ -21,6 +33,9 @@ class Announce(threading.Thread):
         data = str.encode('-'.join([constants.LAN_IDENTIFIER, my_ip]))
 
         while True:
+            if self.stopped():
+                break
+
             s.sendto(data, ('<broadcast>', constants.LAN_PORT))
             time.sleep(5)
 
@@ -38,12 +53,10 @@ class Discover(threading.Thread):
         s.bind(('', constants.LAN_PORT))
 
         while True:
-            data, addr = s.recvfrom(1024)
-
-            data = data.decode()
+            data = s.recv(512)
 
             if data:
-                data = data.split('-')
+                data = data.decode().split('-')
 
                 if len(data) == 2 and data[0] == constants.LAN_IDENTIFIER:
                     print('{} seems to host a Connect Four LAN game'.format(data[1])) # TODO
